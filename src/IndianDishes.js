b9b6ff85c9b50ae2e2afc683d1177d357702dc4f
bhavyashukla16 <73889955+bhavyashukla16@users.noreply.github.com>
2021-01-05 15:41:17 +0530

    Add files via upload

---
+++ src/IndianDishes.js
@@ -0,0 +1,47 @@
+const indianDishes=[
+    {
+        id: 1,
+        name: "Papadoms",
+        description: "With dips",
+        price: "$2.90",
+        tag: "POPULAR"
+    },
+    {
+        id: 2,
+        name: "Garlic Kulcha",
+        description: "vegan curry sauce with eggplant and rice",
+        price: "$2.90",
+        tag: "POPULAR"
+    },
+    {
+        id: 3,
+        name: "Samosa",
+        description: "Two baked dumplings filled with vegetables, with mint sauce",
+        price: "$2.90",
+        tag: "POPULAR"
+    },
+    {
+        id: 4,
+        name: "Mix Pakora",
+        description: "Various vegetables baked in chickpea batter, with dips",
+        price: "$2.90",
+        tag: ""
+    },
+    {
+        id: 5,
+        name: "Daal Makhni",
+        description: "Indian lentils with tomatoes in a finely seasoned curry sauce",
+        price: "$10.30",
+        tag: ""
+    },
+    {
+        id: 6,
+        name: "Chilli Paneer",
+        description: "Homemade cream cheese with bell pepper, onion and garlic in a special red chilli sauce",
+        price: "$11.90",
+        tag: ""
+    }
+];
+
+
+export default indianDishes;
